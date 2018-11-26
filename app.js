e4386e1ae488b65b49e3c0a467bf9a19b0f33602
Tomasz Nowak <tomasz.nowak@itmagination.com>
2018-11-26 16:33:37 +0100

    prototype sketches display switch working

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
+const sketches = new Sketches();
 const state = {
   displayComponent: "sketches",
-  currentSketch: sketch1
+  currentSketch: "sketch1"
 };
+let sketchHandler;
 
-new p5(state.currentSketch, "sketchContainer");
+const drawCurrentSketch = () => {
+  if (sketchHandler) sketchHandler.remove();
+  sketchHandler = new p5(sketches[state.currentSketch], "sketchContainer");
+};
 
 const showGallery = () => {
   const { displayComponent } = state;
   if (displayComponent === "sketches") {
     document.getElementById("sketchContainer").hidden = true;
     document.getElementById("gallery").hidden = false;
-    document.getElementById("switchViewsButton").innerHTML = "Go to gallery";
-    state.displayComponent = 'gallery';
+    document.getElementById("switchViewsButton").innerHTML = "Go to sketches";
+    state.displayComponent = "gallery";
   }
   if (displayComponent === "gallery") {
     document.getElementById("sketchContainer").hidden = false;
     document.getElementById("gallery").hidden = true;
-    document.getElementById("switchViewsButton").innerHTML = "Go to sketch";
-    state.displayComponent = 'sketches';
+    document.getElementById("switchViewsButton").innerHTML = "Go to gallery";
+    state.displayComponent = "sketches";
   }
 };
+
+const selectSketch = sketch => {
+  state.currentSketch = sketch;
+  drawCurrentSketch();
+};
+
+const thumbTemplate = (targetSketch, sketchName, imgUrl) => `
+  <div>
+    <img />
+    <span> Show ${sketchName}</span>
+    <button onclick="selectSketch('${targetSketch}')"> Show this sketch </button>
+  </div>
+`;
+
+const thumbnailsArray = [
+  thumbTemplate("sketch1", "SKETCH 1 🙃", ""),
+  thumbTemplate("sketch2", "SKETCH 2 😒", "")
+];
+
+const thumbnails = thumbnailsArray.reduce(
+  (acc, thumbnail) => `${acc}${thumbnail}`
+);
+
+document.getElementById("gallery").innerHTML = thumbnails;
+
+const init = () => {
+  drawCurrentSketch();
+};
+
+init();
--- sketches.js
+++ sketches.js
@@ -1,28 +1,31 @@
-const sketch1 = context => {
-  const x = 100;
-  const y = 100;
+class Sketches {
+  sketch1(context) {
+    const x = 100;
+    const y = 100;
 
-  context.setup = function() {
-    context.createCanvas(700, 410);
-  };
+    context.setup = function() {
+      context.createCanvas(700, 410);
+    };
 
-  context.draw = function() {
-    context.background(0);
-    context.fill(255);
-    context.rect(x, y, 50, 50);
-  };
-};
-
-
-const sketch2 = context => {
-  const x = 0;
-
-  function setup() {
-    background(100);
+    context.draw = function() {
+      context.background(0);
+      context.fill(255);
+      context.rect(x, y, 50, 50);
+    };
   }
 
-  function draw() {
-    ellipse(x, height / 2, 20, 20);
-    x = x + 1;
+
+  sketch2(context) {
+    let x = 0;
+    const height = 60;
+
+    context.setup = () => {
+      context.background(100);
+    }
+
+    context.draw = () => {
+      context.ellipse(x, height / 2, 20, 20);
+      x = x + 1;
+    }
   }
-};
+}
