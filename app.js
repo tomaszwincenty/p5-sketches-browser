4e3ab849e4fe73ab774cb96ae097aa924ed47f0f
Tomasz Nowak <tomasz.nowak@itmagination.com>
2018-11-29 11:50:55 +0100

    simplyfy codes and add some basic styles

--- app.js
+++ app.js
@@ -1,57 +1,17 @@
-const sketches = new Sketches();
-const state = {
-  displayComponent: "sketches",
-  currentSketch: "sketch1"
-};
-let sketchHandler;
-
-const drawCurrentSketch = () => {
-  if (sketchHandler) sketchHandler.remove();
-  sketchHandler = new p5(sketches[state.currentSketch], "sketchContainer");
+const drawCurrentSketch = (selectedSketch) => {
+  const container = document.getElementById("sketch-iframe");
+  const src = document.createAttribute("src");
+  src.value = `./sketches/${selectedSketch}.html`;
+  container.setAttributeNode(src);
 };
 
-const showGallery = () => {
-  const { displayComponent } = state;
-  if (displayComponent === "sketches") {
-    document.getElementById("sketchContainer").hidden = true;
-    document.getElementById("gallery").hidden = false;
-    document.getElementById("switchViewsButton").innerHTML = "Go to sketches";
-    state.displayComponent = "gallery";
-  }
-  if (displayComponent === "gallery") {
-    document.getElementById("sketchContainer").hidden = false;
-    document.getElementById("gallery").hidden = true;
-    document.getElementById("switchViewsButton").innerHTML = "Go to gallery";
-    state.displayComponent = "sketches";
-  }
+const onShowGallery = () => {
+  document.getElementById("sketch-container").hidden = true;
+  document.getElementById("gallery").hidden = false;
 };
 
-const selectSketch = sketch => {
-  state.currentSketch = sketch;
-  drawCurrentSketch();
+const onSelectSketch = (selectedSketch) => {
+  drawCurrentSketch(selectedSketch);
+  document.getElementById("sketch-container").hidden = false;
+  document.getElementById("gallery").hidden = true;
 };
-
-const thumbTemplate = (targetSketch, sketchName, imgUrl) => `
-  <div>
-    <img />
-    <span> Show ${sketchName}</span>
-    <button onclick="selectSketch('${targetSketch}')"> Show this sketch </button>
-  </div>
-`;
-
-const thumbnailsArray = [
-  thumbTemplate("sketch1", "SKETCH 1 🙃", ""),
-  thumbTemplate("sketch2", "SKETCH 2 😒", "")
-];
-
-const thumbnails = thumbnailsArray.reduce(
-  (acc, thumbnail) => `${acc}${thumbnail}`
-);
-
-document.getElementById("gallery").innerHTML = thumbnails;
-
-const init = () => {
-  drawCurrentSketch();
-};
-
-init();
